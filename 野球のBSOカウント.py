673874af84fcfaf9f548d61aad650a0d5b55dce5
junpei onizawa <onizawajunpei4@gmail.com>
2019-06-25 01:10:06 +0900

    野球のBSOカウント

---
+++ 野球のBSOカウント.py
@@ -0,0 +1,23 @@
+s1 = ""
+sk = 0
+bl = 0
+import random
+for N in range(1,7):
+ if  N  <= 7:
+ 	print(N)
+ 	mylist = ["ストライク！！", "ボール！！"]
+ 	s1 = random.choice(mylist)
+ 	print(s1)
+ if s1 == "strike!":
+ 	sk += 1
+ else:
+ 	bl += 1
+ if sk > 2:
+ 	print("三振!!アウト!!")
+ 	break
+ if bl > 3:
+ 	print("フォアボール!!")
+ 	break
+
+
+
